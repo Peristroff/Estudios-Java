8a6c6f6d23f1da20aeceab6ae6b2afbb2c4d3727
Peristroff <salas.pablo.96@gmail.com>
2023-10-30 08:12:50 -0300

    Creación de repo
    Archivos subidos entregados por Jerry y creación de repo.

---
+++ src/tarea2/Conexion.java
@@ -0,0 +1,37 @@
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    String bd ="registro";
+    String url="jdbc:mysql://localhost:3306/";
+    String user="root";
+    String password="";
+    String driver="com.mysql.cj.jdbc.Driver";
+    Connection con;
+
+    public Conexion(String bd){
+        this.bd = bd;
+        try{
+            Class.forName(driver);
+            con=DriverManager.getConnection(url+this.bd,user,password);
+            System.out.println("Se conecto a "+bd);
+        } catch (Exception ex){
+            //java.util.logging.Logger.getLogger(Conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            System.out.println("No se conecto a "+bd);
+        }
+    }
+    public Connection getConnection(){
+        return con;
+    }
+    public void desconectar(){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            java.util.logging.Logger.getLogger(Conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+    
+}
