9131de53bebd940a28d38f14a004c14745e215e8
Peristroff <salas.pablo.96@gmail.com>
2023-11-09 19:29:46 -0300

    gitignore cambiado y UI cambiado

--- src/tarea2/Conexion.java
+++ src/tarea2/Conexion.java
@@ -9,27 +9,24 @@ import java.sql.SQLException;
 public class Conexion {
     String bd ="registro";
     String url="jdbc:mysql://localhost:3306/";
+    // String url="jdbc:mysql://127.0.0.1:3306/";
     String user="root";
-    String password="";
+    String password="root";
     String driver="com.mysql.cj.jdbc.Driver";
     Connection conexion = null;
     Connection con;
 
     public Connection conectar(String bd){
-        this.bd = bd;
+        System.out.println(bd);
         try{
             Class.forName(driver);
-            con = DriverManager.getConnection(url+this.bd,user,password);
+            con = DriverManager.getConnection(url + bd, user, password);
             System.out.println("Se conecto a "+bd);
         } catch (Exception ex){
             // java.util.logging.Logger.getLogger(Conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             System.out.println("No se conecto a "+bd);
         }
         return conexion;
-    }
-
-    public Connection getConnection(){
-        return con;
     }
 
     public void desconectar(){
